49f4b7b810131c7e053a819b9cd9c095eb94756b
Mr-longhu <2813046848@qq.com>
2019-12-13 15:27:36 +0800

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import { get, post } from "./http";
 let api = {
-  login: params => post("/admin/login", params), //登录
-  logout: params => post("/admin/logout", params), //退出登录
-  logout: params => post("/admin/logout", params), //退出登录
-  addRegion: params => post("/admin/RegionManager/addRegion", params), //添加区域经理
-  regionManagerDetail: params => post("/admin/RegionManager/regionManagerDetail", params), //区域经理详情
-  delRegion: params => post("/admin/RegionManager/delRegion", params), //注销区域经理
-  regionList: params => post("/admin/RegionManager/regionList", params) //区域经理列表
+  login: params => post("/login", params), //登录
+  logout: params => post("/logout", params), //退出登录
+  logout: params => post("/logout", params), //退出登录
+  addRegion: params => post("/RegionManager/addRegion", params), //添加区域经理
+  regionManagerDetail: params => post("/RegionManager/regionManagerDetail", params), //区域经理详情
+  delRegion: params => post("/RegionManager/delRegion", params), //注销区域经理
+  regionList: params => post("/RegionManager/regionList", params) //区域经理列表
 };
 export default api;
