3afdc39922fdae33b43614e7932fb41c7f2bad2e
Mr-longhu <2813046848@qq.com>
2019-12-13 15:28:00 +0800

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,6 @@ import { get, post } from "./http";
 let api = {
   login: params => post("/login", params), //登录
   logout: params => post("/logout", params), //退出登录
-  logout: params => post("/logout", params), //退出登录
   addRegion: params => post("/RegionManager/addRegion", params), //添加区域经理
   regionManagerDetail: params => post("/RegionManager/regionManagerDetail", params), //区域经理详情
   delRegion: params => post("/RegionManager/delRegion", params), //注销区域经理
