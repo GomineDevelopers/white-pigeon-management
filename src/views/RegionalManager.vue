1e20b0c09a6b6656ed6b8f2635e0277fdfa3354d
Mr-longhu <2813046848@qq.com>
2019-12-13 18:12:07 +0800

    commit

--- src/views/RegionalManager.vue
+++ src/views/RegionalManager.vue
@@ -7,37 +7,18 @@
       <el-col :span="20">
         <div class="main_header_item">
           <span>姓名：</span>
-          <el-input size="small" v-model="user" placeholder="请输入"></el-input>
+          <el-input size="small" v-model="managerName" placeholder="请输入"></el-input>
         </div>
         <div class="main_header_item">
-          <span>省：</span>
+          <span>省/市：</span>
           <el-cascader
-            size="large"
-            :options="provinceOptions"
-            v-model="provinceSelect"
+            size="small"
+            :options="provinceAndCityDataPlus"
+            v-model="searchOption"
             @change="handleChange"
           >
           </el-cascader>
-          <!-- <el-select size="small" v-model="province" placeholder="请选择">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select> -->
         </div>
-        <!-- <div class="main_header_item">
-          <span>市：</span>
-          <el-select size="small" v-model="city" placeholder="请选择">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </div> -->
       </el-col>
       <el-col :span="4" class="main_header_btns">
         <el-button size="small" type="primary" @click="search">搜索</el-button>
@@ -54,11 +35,11 @@
         <el-button size="small" plain icon="el-icon-delete" @click="deleteselected">删除</el-button>
       </div>
       <el-table
-        :data="tableData"
+        :data="list"
         v-loading="listLoading"
-        element-loading-text="数据拼命加载中"
+        element-loading-text="数据拼命加载中..."
         element-loading-spinner="el-icon-loading"
-        element-loading-background="rgba(0, 0, 0, 0.8)"
+        element-loading-background="rgba(0, 0, 0, 0.6)"
         @selection-change="selectionChange"
         style="width: 100%"
       >
@@ -103,7 +84,11 @@
         <li><label>市：</label>{{ singleData.city_name }}</li>
         <li><label>姓名：</label>{{ singleData.name }}</li>
         <li><label>手机号：</label>{{ singleData.phone }}</li>
-        <li><label>状态：</label>{{ singleData.status ? formatStatus(singleData.status) : "" }}</li>
+        <li>
+          <label>状态：</label>
+          <span v-if="singleData.status == 1">正常</span>
+          <span v-else class="logout">注销</span>
+          </li>
       </ul>
       <div class="dialog_title" slot="title"><span class="line"></span>经理信息</div>
       <div slot="footer" class="dialog-footer">
@@ -114,32 +99,39 @@
     <!-- 新增经理 -->
     <el-dialog class="dialog_wrap" :visible.sync="addVisble" :append-to-body="true">
       <div class="dialog_title" slot="title"><span class="line"></span>新增经理</div>
-      <el-form :model="addManagerData" :rules="rules" ref="ruleForm" label-width="100px">
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="省：" prop="province_code">
-              <el-select size="small" v-model="addManagerData.province_code" placeholder="请选择省">
+      <el-form :model="managerData" :rules="rules" ref="ruleForm" label-width="100px">
+        <!-- <el-row>
+          <el-col :span="12"> -->
+            <el-form-item label="省：" prop="option">
+              <el-cascader
+                size="small"
+                :options="provinceAndCityData"
+                v-model="managerData.option"
+                @change="handleManagerChange"
+              >
+              </el-cascader>
+              <!-- <el-select size="small" v-model="managerData.province_code" placeholder="请选择省">
                 <el-option label="浙江省" value="zj"></el-option>
                 <el-option label="安徽省" value="ah"></el-option>
-              </el-select>
+              </el-select> -->
             </el-form-item>
-          </el-col>
-          <el-col :span="12">
+          <!-- </el-col> -->
+          <!-- <el-col :span="12">
             <el-form-item size="small" label="市：" prop="city_code">
-              <el-select v-model="addManagerData.city_code" placeholder="请选择市">
+              <el-select v-model="managerData.city_code" placeholder="请选择市">
                 <el-option label="贵阳" value="gy"></el-option>
                 <el-option label="遵义" value="zy"></el-option>
               </el-select>
             </el-form-item>
-          </el-col>
-        </el-row>
+          </el-col> -->
+        <!-- </el-row> -->
         <el-form-item label="姓名：" prop="name">
-          <el-input size="small" v-model="addManagerData.name" placeholder="请输入"></el-input>
+          <el-input size="small" v-model="managerData.name" placeholder="请输入"></el-input>
         </el-form-item>
         <el-form-item label="手机号：" prop="email">
           <el-input
             size="small"
-            v-model.number="addManagerData.email"
+            v-model.number="managerData.email"
             placeholder="请输入"
           ></el-input>
         </el-form-item>
@@ -147,7 +139,7 @@
           <el-input
             size="small"
             type="password"
-            v-model="addManagerData.password"
+            v-model="managerData.password"
             placeholder="请输入"
           ></el-input>
         </el-form-item>
@@ -160,7 +152,7 @@
   </div>
 </template>
 <script>
-import { provinceAndCityData } from "element-china-area-data";
+import { provinceAndCityDataPlus, provinceAndCityData, CodeToText } from "element-china-area-data";
 export default {
   name: "RegionalManager",
   data() {
@@ -183,43 +175,20 @@ export default {
       detailVisble: false, //详情弹窗
       singleData: {}, //单条数据
       selected: [], //选中数据
-      user: null, // 搜索姓名
-      provinceOptions: provinceAndCityData, //省市数据
-      provinceSelect: "", //存放省市选择值
-      province: null, // 搜索省
-      // city: null, // 搜索市
-      addManagerData: {
-        province_code: null,
-        city_code: null,
+      managerName: null, // 搜索姓名
+      provinceAndCityDataPlus: provinceAndCityDataPlus, //省市数据带“全部”
+      provinceAndCityData: provinceAndCityData, //省市数据不带“全部”
+      searchOption: [], //搜索省市
+      isSearch: false, //是否是搜索请求
+      managerData: {
+        option: [],
         name: null,
         email: null,
         password: null
       }, //新增数据
       page: 1,
       row: 10,
-      options: [
-        {
-          value: "选项1",
-          label: "黄金糕"
-        },
-        {
-          value: "选项2",
-          label: "双皮奶"
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎"
-        },
-        {
-          value: "选项4",
-          label: "龙须面"
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭"
-        }
-      ],
-      tableData: [
+      list: [
         {
           id: 1,
           province_name: "浙江省",
@@ -311,8 +280,7 @@ export default {
       ],
       rules: {
         name: [{ required: true, message: "请输入姓名" }],
-        province_code: [{ required: true, message: "请选择省份" }],
-        city_code: [{ required: true, message: "请选择市" }],
+        option: [{ required: true, message: "请选择省市" }],
         email: [{ required: true, validator: checkPhone, trigger: "blur" }],
         password: [{ required: true, message: "请输入密码" }]
       }
@@ -325,39 +293,57 @@ export default {
   },
   methods: {
     //地区省市操作
-    handleChange(value) {
-      console.log(value);
+    handleChange(arr) {
+      this.searchOption = arr;
+    },
+    // 新增/编辑经理省市选择
+    handleManagerChange(arr) {
+      this.managerData.option = arr;
     },
     // 获取列表数据
     getListData() {
-      let params = {
-        page: this.page,
-        row: this.row
-      };
+      this.listLoading = true;
+      let params = null;
+      if (this.isSearch){
+        params = {
+          regional_manager_name: this.managerName,
+          province_code: this.searchOption[0],
+          city_code: this.searchOption[1],
+          page: this.page,
+          row: this.row
+        }
+      } else {
+        params = {
+          page: this.page,
+          row: this.row
+        };
+      }
+          console.log(params);
       this.$api.regionList(params)
         .then( res => {
           console.log(res)
+          // this.list = res
+          this.listLoading = false;
         })
         .catch( err => {
+          this.listLoading = false;
           console.log(err)
         })
-      console.log(params);
       // this.listLoading = true;
     },
-    // 状态转换
-    formatStatus(row, column) {
-      return row.status == 1 ? "正常" : "注销";
-    },
+    
     // 搜索
     search() {
-      if (this.user == null && this.province == null && this.city == null) {
+      if (this.managerName == null && !this.searchOption.length) {
         this.$message({
           message: "请输入或选择搜索内容",
           type: "error"
         });
         return false;
       }
-      console.log(this.user, this.province, this.city);
+      this.isSearch = true;
+      this.page = 1;
+      this.getListData();
     },
     // 重置搜索内容
     resetSearch() {
@@ -383,7 +369,8 @@ export default {
         })
         .then(() => {
           console.log(row.id);
-          let para = { id: row.id };
+          let params = { id: row.id };
+          this.delRegion(params);
         })
         .catch(() => {
           console.log("取消");
@@ -394,6 +381,7 @@ export default {
     selectionChange(sels) {
       this.selected = sels;
     },
+
     // 删除选中选项
     deleteselected() {
       if (!this.selected.length) {
@@ -414,17 +402,20 @@ export default {
           console.log("取消");
         });
     },
+
     // 点击分页当前页数
     currentChange(val) {
       this.page = val;
       this.getListData();
       console.log(val);
     },
+
     // 切换每页条数
     sizeChange(val) {
       this.row = val;
       console.log(val);
     },
+
     // 新增经理
     addManager(formName) {
       this.$refs[formName].validate(valid => {
@@ -435,9 +426,35 @@ export default {
         }
       });
     },
+
+    // 注销区域经理
+    delRegion(params) {
+      this.$api.delRegion(params)
+        .then( res => {
+          console.log(res)
+        })
+        .catch( err => {
+          console.log(err)
+        })
+    },
+
     // 提交数据
     submitManager() {
-      console.log(this.addManagerData);
+      let params = {
+        province_code: this.managerData.option[0],
+        city_code: this.managerData.option[1],
+        name: this.managerData.name,
+        email: this.managerData.email,
+        password: this.managerData.password
+      };
+          console.log(params);
+      this.$api.addRegion(params)
+        .then( res => {
+          console.log(res)
+        })
+        .catch( err => {
+          console.log(err)
+        })
     }
   }
 };
