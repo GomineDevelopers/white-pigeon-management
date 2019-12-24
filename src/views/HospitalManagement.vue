83e5a32f3d0e2a3e9e069276b5db0ad96e146b59
Mr-longhu <2813046848@qq.com>
2019-12-24 15:53:23 +0800

    commit
    资料中心增加PDF预览功能

--- src/views/HospitalManagement.vue
+++ src/views/HospitalManagement.vue
@@ -12,52 +12,34 @@
             :options="provinceAndCityDataPlus"
             v-model="searchOption"
             @change="handleChange"
-          >
-          </el-cascader>
+          ></el-cascader>
         </div>
         <div class="main_header_item">
           <span>医院：</span>
-          <el-input size="small" v-model="user" placeholder="请输入"></el-input>
+          <el-input size="small" v-model="hospitalId" placeholder="请输入"></el-input>
         </div>
         <div class="main_header_item">
           <span>等级：</span>
-          <el-select size="small" v-model="city" placeholder="请选择">
+          <el-select size="small" v-model="gradeId" placeholder="请选择">
             <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+              v-for="item in gradeArr"
+              :key="item.id"
+              :label="item.grade_name"
+              :value="item.id"
             ></el-option>
           </el-select>
         </div>
       </el-col>
       <el-col :span="4" class="main_header_btns">
         <el-button size="small" type="primary" @click="search">搜索</el-button>
-        <el-button size="small" type="primary" @click="resetSearch" plain
-          >重置</el-button
-        >
+        <el-button size="small" type="primary" @click="resetSearch" plain>重置</el-button>
       </el-col>
     </el-row>
     <!-- 列表 -->
     <div class="main_list">
       <div class="toolbar">
-        <el-button
-          size="small"
-          plain
-          icon="el-icon-plus"
-          @click="addVisble = true"
-          >新增医院</el-button
-        >
-        <el-button size="small" plain icon="el-icon-bottom" @click="downLoad"
-          >下载</el-button
-        >
-        <el-button
-          size="small"
-          plain
-          icon="el-icon-delete"
-          @click="deleteselected"
-          >删除</el-button
-        >
+        <el-button size="small" plain icon="el-icon-plus" @click="handleCreate">新增医院</el-button>
+        <el-button size="small" plain icon="el-icon-bottom" @click="downLoad">下载</el-button>
       </div>
       <el-table
         :data="tableData"
@@ -65,74 +47,24 @@
         element-loading-text="数据拼命加载中"
         element-loading-spinner="el-icon-loading"
         element-loading-background="rgba(0, 0, 0, 0.8)"
-        @selection-change="selectionChange"
         style="width: 100%"
       >
-        <el-table-column type="selection" width="60"></el-table-column>
-        <el-table-column
-          prop="hospital_num"
-          label="医院编号"
-          width="120"
-        ></el-table-column>
-        <el-table-column
-          prop="hospital_name"
-          label="医院名称"
-          min-width="260"
-        ></el-table-column>
-        <el-table-column
-          prop="hospital_rank"
-          label="医院等级"
-          width="120"
-        ></el-table-column>
-        <el-table-column
-          prop="business"
-          label="经营方式"
-          width="120"
-        ></el-table-column>
-        <el-table-column
-          prop="phone"
-          label="联系方式"
-          width="160"
-        ></el-table-column>
-        <el-table-column
-          prop="net"
-          label="医院网址"
-          width="240"
-        ></el-table-column>
+        <el-table-column prop="hospital_num" label="医院编号" width="120"></el-table-column>
+        <el-table-column prop="hospital_name" label="医院名称" min-width="260"></el-table-column>
+        <el-table-column prop="hospital_rank" label="医院等级" width="120"></el-table-column>
+        <el-table-column prop="business" label="经营方式" width="120"></el-table-column>
+        <el-table-column prop="phone" label="联系方式" width="160"></el-table-column>
+        <el-table-column prop="net" label="医院网址" width="240"></el-table-column>
         <el-table-column label="操作" width="120" fixed="right">
           <template scope="scope">
-            <el-tooltip
-              class="item"
-              effect="dark"
-              content="查看"
-              placement="top"
-            >
-              <i
-                class="el-icon-view"
-                @click="handleDetail(scope.$index, scope.row)"
-              ></i>
+            <el-tooltip class="item" effect="dark" content="查看" placement="top">
+              <i class="el-icon-view" @click="handleDetail(scope.$index, scope.row)"></i>
             </el-tooltip>
-            <el-tooltip
-              class="item"
-              effect="dark"
-              content="编辑"
-              placement="top"
-            >
-              <i
-                class="el-icon-edit"
-                @click="handleEdit(scope.$index, scope.row)"
-              ></i>
+            <el-tooltip class="item" effect="dark" content="编辑" placement="top">
+              <i class="el-icon-edit" @click="handleEdit(scope.$index, scope.row)"></i>
             </el-tooltip>
-            <el-tooltip
-              class="item"
-              effect="dark"
-              content="删除"
-              placement="top"
-            >
-              <i
-                class="el-icon-delete"
-                @click="handleDelete(scope.$index, scope.row)"
-              ></i>
+            <el-tooltip class="item" effect="dark" content="删除" placement="top">
+              <i class="el-icon-delete" @click="handleDelete(scope.$index, scope.row)"></i>
             </el-tooltip>
           </template>
         </el-table-column>
@@ -145,126 +77,100 @@
           @current-change="currentChange"
           @size-change="sizeChange"
           :total="1000"
-        >
-        </el-pagination>
+        ></el-pagination>
       </div>
     </div>
     <!-- 详情弹窗 -->
-    <el-dialog
-      class="dialog_wrap"
-      :visible.sync="detailVisble"
-      :append-to-body="true"
-      width="30%"
-    >
+    <el-dialog class="dialog_wrap" :visible.sync="detailVisble" :append-to-body="true" width="30%">
       <div class="dialog_title" slot="title">
         <span class="line"></span>医院信息
       </div>
       <ul class="dialog_detail">
-        <li><label>医院编号：</label>{{ singleData.hospital_num }}</li>
-        <li><label>医院名称：</label>{{ singleData.hospital_name }}</li>
-        <li><label>医院等级：</label>{{ singleData.hospital_rank }}</li>
-        <li><label>经营方式：</label>{{ singleData.business }}</li>
-        <li><label>医院网址：</label>{{ singleData.net }}</li>
-        <li><label>联系方式：</label>{{ singleData.phone }}</li>
         <li>
-          <label>所在省市：</label>{{ singleData.province_name }}
+          <label>医院编号：</label>
+          {{ singleData.hospital_num }}
+        </li>
+        <li>
+          <label>医院名称：</label>
+          {{ singleData.hospital_name }}
+        </li>
+        <li>
+          <label>医院等级：</label>
+          {{ singleData.hospital_rank }}
+        </li>
+        <li>
+          <label>经营方式：</label>
+          {{ singleData.business }}
+        </li>
+        <li>
+          <label>医院网址：</label>
+          {{ singleData.net }}
+        </li>
+        <li>
+          <label>联系方式：</label>
+          {{ singleData.phone }}
+        </li>
+        <li>
+          <label>所在省市：</label>
+          {{ singleData.province_name }}
           {{ singleData.city_name }}
         </li>
-        <li><label>详细地址：</label>{{ singleData.address }}</li>
         <li>
-          <span><label>经度：</label>{{ singleData.lng }}</span>
-          <span><label>纬度：</label>{{ singleData.lat }}</span>
+          <label>详细地址：</label>
+          {{ singleData.address }}
+        </li>
+        <li>
+          <span>
+            <label>经度：</label>
+            {{ singleData.lng }}
+          </span>
+          <span>
+            <label>纬度：</label>
+            {{ singleData.lat }}
+          </span>
         </li>
       </ul>
       <div slot="footer" class="dialog-footer">
-        <el-button size="small" type="primary" @click="detailVisble = false"
-          >确 定</el-button
-        >
-        <el-button size="small" type="info" plain @click="detailVisble = false"
-          >取 消</el-button
-        >
+        <el-button size="small" type="primary" @click="detailVisble = false">确 定</el-button>
+        <el-button size="small" type="info" plain @click="detailVisble = false">取 消</el-button>
       </div>
     </el-dialog>
     <!-- 新增 -->
-    <el-dialog
-      class="dialog_wrap"
-      :visible.sync="addVisble"
-      :append-to-body="true"
-    >
+    <el-dialog class="dialog_wrap" :visible.sync="addVisble" :append-to-body="true">
       <div class="dialog_title" slot="title">
         <span class="line"></span>新增医院
       </div>
-      <el-form
-        v-if="addVisble"
-        :model="addData"
-        :rules="rules"
-        ref="ruleForm"
-        label-width="100px"
-      >
+      <el-form v-if="addVisble" :model="addData" :rules="rules" ref="ruleForm" label-width="100px">
         <el-form-item label="医院编号：" prop="hospital_num">
-          <el-input
-            size="small"
-            v-model="addData.hospital_num"
-            placeholder="请输入"
-          ></el-input>
+          <el-input size="small" v-model="addData.hospital_num" placeholder="请输入"></el-input>
         </el-form-item>
-        <el-form-item
-          label="医院名称："
-          prop="hospital_name"
-          class="width_full"
-        >
-          <el-select
-            size="small"
-            v-model="addData.hospital_name"
-            placeholder="请选择医院名称"
-          >
-            <el-option
-              label="复旦大学附属眼耳鼻喉科医院"
-              value="复旦大学附属眼耳鼻喉科医院"
-            ></el-option>
-            <el-option
-              label="附属眼耳鼻喉科医院"
-              value="附属眼耳鼻喉科医院"
-            ></el-option>
+        <el-form-item label="医院名称：" prop="hospital_name" class="width_full">
+          <el-select size="small" v-model="addData.hospital_name" placeholder="请选择医院名称">
+            <el-option label="复旦大学附属眼耳鼻喉科医院" value="复旦大学附属眼耳鼻喉科医院"></el-option>
+            <el-option label="附属眼耳鼻喉科医院" value="附属眼耳鼻喉科医院"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item
-          label="医院等级："
-          prop="hospital_rank"
-          class="width_full"
-        >
-          <el-select
-            size="small"
-            v-model="addData.hospital_rank"
-            placeholder="请选择医院等级"
-          >
-            <el-option label="三级甲等" value="三级甲等"></el-option>
-            <el-option label="三级乙等" value="三级乙等"></el-option>
+        <el-form-item label="医院等级：" prop="hospital_rank" class="width_full">
+          <el-select size="small" v-model="addData.hospital_rank" placeholder="请选择医院等级">
+            <el-option
+              v-for="item in gradeArr"
+              :label="item.grade_name"
+              :value="item.id"
+              :key="item.id"
+            ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="经营方式：" prop="business" class="width_full">
-          <el-select
-            size="small"
-            v-model="addData.business"
-            placeholder="请选择经营方式"
-          >
+          <el-select size="small" v-model="addData.business" placeholder="请选择经营方式">
             <el-option label="公立" value="公立"></el-option>
             <el-option label="私立" value="私立"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="医院网址：" prop="net">
-          <el-input
-            size="small"
-            v-model="addData.net"
-            placeholder="请输入"
-          ></el-input>
+          <el-input size="small" v-model="addData.net" placeholder="请输入"></el-input>
         </el-form-item>
         <el-form-item label="联系方式：" prop="phone">
-          <el-input
-            size="small"
-            v-model="addData.phone"
-            placeholder="请输入"
-          ></el-input>
+          <el-input size="small" v-model="addData.phone" placeholder="请输入"></el-input>
         </el-form-item>
         <el-form-item label="省/市：" prop="option">
           <el-cascader
@@ -273,44 +179,27 @@
             :options="provinceAndCityData"
             v-model="addData.option"
             @change="handleManagerChange"
-          >
-          </el-cascader>
+          ></el-cascader>
         </el-form-item>
         <el-form-item label="详细地址：" prop="address">
-          <el-input
-            size="small"
-            v-model="addData.address"
-            placeholder="请输入"
-          ></el-input>
+          <el-input size="small" v-model="addData.address" placeholder="请输入"></el-input>
         </el-form-item>
         <el-row>
           <el-col :span="12">
             <el-form-item label="纬度：" prop="lng">
-              <el-input
-                size="small"
-                v-model="addData.lng"
-                placeholder="请输入"
-              ></el-input>
+              <el-input size="small" v-model="addData.lng" placeholder="请输入"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
             <el-form-item label="经度：" prop="lat">
-              <el-input
-                size="small"
-                v-model="addData.lat"
-                placeholder="请输入"
-              ></el-input>
+              <el-input size="small" v-model="addData.lat" placeholder="请输入"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button size="small" type="primary" @click="addManager('ruleForm')"
-          >确 定</el-button
-        >
-        <el-button size="small" type="info" plain @click="addVisble = false"
-          >取 消</el-button
-        >
+        <el-button size="small" type="primary" @click="addManager('ruleForm')">确 定</el-button>
+        <el-button size="small" type="info" plain @click="addVisble = false">取 消</el-button>
       </div>
     </el-dialog>
   </div>
@@ -341,12 +230,12 @@ export default {
       addVisble: false, //新增
       detailVisble: false, //详情弹窗
       singleData: {}, //单条数据
-      selected: [], //选中数据
-      user: null, // 搜索姓名
       provinceAndCityDataPlus: provinceAndCityDataPlus, //省市数据带“全部”
       provinceAndCityData: provinceAndCityData, //省市数据不带“全部”
-      searchOption: [], //搜索省市
       isSearch: false, //是否是搜索请求
+      searchOption: [], //搜索省市
+      gradeId: null, //搜索等级
+      hospitalId: null, //搜索医院
       addData: {
         // option: [],
         // hospital_num: null,
@@ -363,27 +252,17 @@ export default {
       }, //新增数据
       page: 1,
       row: 10,
-      options: [
-        {
-          value: "选项1",
-          label: "黄金糕"
-        },
-        {
-          value: "选项2",
-          label: "双皮奶"
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎"
-        },
-        {
-          value: "选项4",
-          label: "龙须面"
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭"
-        }
+      gradeArr: [
+        { id: 1, grade_name: "三级甲等" },
+        { id: 2, grade_name: "三级乙等" },
+        { id: 3, grade_name: "三级丙等" },
+        { id: 4, grade_name: "二级甲等" },
+        { id: 5, grade_name: "二级乙等" },
+        { id: 6, grade_name: "二级丙等" },
+        { id: 7, grade_name: "一级甲等" },
+        { id: 8, grade_name: "一级乙等" },
+        { id: 9, grade_name: "一级丙等" },
+        { id: 10, grade_name: "未知" }
       ],
       tableData: [
         {
@@ -532,8 +411,7 @@ export default {
       ],
       rules: {
         hospital_num: { required: true, message: "请输入医院编号" },
-        province_code: { required: true, message: "请选择省份" },
-        city_code: { required: true, message: "请选择市" },
+        option: { required: true, message: "请选择省市" },
         hospital_name: { required: true, message: "请输入医院名称" },
         hospital_rank: { required: true, message: "请选择医院等级" },
         business: { required: true, message: "请选择经营方式" },
@@ -570,20 +448,24 @@ export default {
     },
     // 搜索
     search() {
-      if (this.user == null && !this.searchOption.length) {
+      if (
+        this.hospitalId == null &&
+        !this.searchOption.length &&
+        this.gradeId == null
+      ) {
         this.$message({
           message: "请输入或选择搜索内容",
           type: "error"
         });
         return false;
       }
-      console.log(this.user, this.province, this.city);
+      console.log(this.hospitalId, this.searchOption, this.gradeId);
     },
     // 重置搜索内容
     resetSearch() {
-      this.user = null;
-      this.province = null;
-      this.city = null;
+      this.searchOption = [];
+      this.gradeId = null;
+      this.hospitalId = null;
     },
     // 下载
     downLoad() {
@@ -610,32 +492,17 @@ export default {
         });
       //   console.log(index, row);
     },
-    // 选择框选择
-    selectionChange(sels) {
-      this.selected = sels;
+
+    // 新增科室
+    handleCreate() {
+      this.isEdit = false;
+      this.addData = {};
+      this.addVisble = true;
     },
-    // 删除选中选项
-    deleteselected() {
-      if (!this.selected.length) {
-        this.$message({
-          message: "未选中数据",
-          type: "error"
-        });
-        return false;
-      }
-      this.$messageBox
-        .confirm("确认删除所选记录吗?", "提示", {
-          type: "warning"
-        })
-        .then(() => {
-          console.log(this.selected);
-        })
-        .catch(() => {
-          console.log("取消");
-        });
-    },
+
     // 编辑医院
     handleEdit(index, row) {
+      this.isSearch = true;
       this.addVisble = true;
       this.addData = JSON.parse(JSON.stringify(row));
       console.log(row);
