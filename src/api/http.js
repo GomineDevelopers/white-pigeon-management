49ca78346e0cb3188dae51636d59789575730cca
Mr-longhu <2813046848@qq.com>
2019-12-27 09:04:59 +0800

    Update http.js

--- src/api/http.js
+++ src/api/http.js
@@ -13,7 +13,8 @@ import { Message } from "element-ui";
 // }
 
 axios.defaults.timeout = 10000; //设置请求超时
-axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded;charset=UTF-8"; //设置post请求头
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded;charset=UTF-8"; //设置post请求头
 
 // 请求拦截
 axios.interceptors.request.use(
